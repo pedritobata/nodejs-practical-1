2838a1c3f67f929da603b13998a2ba9819ab1c4c
unknown <pedromartinez_goyo@hotmail.com>
2020-06-15 18:17:32 -0500

    server configuration completed first part

--- imgshare/src/server/config.js
+++ imgshare/src/server/config.js
@@ -1,9 +1,43 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+const errorHandler = require('errorhandler');
+const routes = require('../routes/index');
+
 module.exports = app => {
 
     //settings
     app.set('port', process.env.PORT || 3000);
 
-    
+    //motor de renderizado
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }));
+    app.set('view engine', '.hbs');
+
+    //Middlewares
+    app.use(morgan('dev'));
+    app.use(multer({ dest: path.join(__dirname, '../public/upload/temp') }).single('image'));
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+    // Routes
+    routes(app);
+
+    //static files
+    app.use('/public',express.static(path.join(__dirname, '../public')));
+
+    //Error Handlers
+    if('development' ===  app.get('env')){//env es una variable de entorno del servidor
+        app.use(errorHandler);
+    }
 
 
     return app;
