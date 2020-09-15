7bcac4509db5e447d08ca980f6618e57a650db82
unknown <pedromartinez_goyo@hotmail.com>
2020-09-14 19:06:55 -0500

    installing axios on posts ms

---
+++ blog/posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+    res.status(200).send(posts);
+});
+
+app.post("posts", (req, res) =>{
+    const id = randomBytes(4).toString("hex");
+    const {title} = req.body;
+    posts[id] = {
+        id, title
+    }
+});
+
+
+app.listen(4000, () => {
+    console.log("posts service running on port 4000...");
+})
