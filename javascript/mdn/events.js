6c5117e5ebe121faa3d61a233e52f2250c11fb43
pedro martinez <pedromartinez_goyo@hotmail.com>
2021-04-17 18:55:47 -0500

    Events introduction finished up

--- javascript/mdn/events.js
+++ javascript/mdn/events.js
@@ -3,28 +3,60 @@ c('Introduction to Events!!');
 
 // Utils
 const getRandomNumber = (number) => {
-    return Math.floor(Math.random() * (number + 1));
-}
+  return Math.floor(Math.random() * (number + 1));
+};
+
+const getRandomColor = () => `rgb(${getRandomNumber(255)}, ${getRandomNumber(
+    255
+  )}, ${getRandomNumber(255)}`;
 
 // Example #1
 
 {
-    const btn = document.querySelector('.ex1 button');
+  const btn = document.querySelector('.ex1 button');
 
-    function changeBox1Color(event) {
-        const randomColor = `rgb(${getRandomNumber(255)}, ${getRandomNumber(255)}, ${getRandomNumber(255)}`;
-        event.target.parentElement.style.backgroundColor = randomColor;
+  function changeBox1Color(event) {
+    event.target.parentElement.style.backgroundColor = getRandomColor();
+  }
+
+  btn.onclick = changeBox1Color;
+  //btn.onkeypress = changeBox1Color;  no existe ese evento para elementos html
+  /*  btn.onfocus = changeBox1Color;
+  btn.onblur = changeBox1Color;
+  btn.onmouseenter = changeBox1Color;
+  btn.onmouseleave = changeBox1Color; */
+
+  //En el window
+  //window.onkeypress = changeBox1Color;
+  //window.onkeydown = changeBox1Color;
+  //window.onkeyup = changeBox1Color;
+}
+
+// Example #2
+{
+  const btn = document.querySelector('.ex2 button');
+  const messageHandler = function () {
+    const message = 'Segundo evento lanzado!!';
+    const messageEl = document.querySelector('.ex2 > p');
+    messageEl.textContent = message;
+  };
+  const alertHandler = function () {
+    alert('Primer evento lanzado!!');
+  };
+  btn.addEventListener('click', messageHandler);
+  btn.addEventListener('click', alertHandler); // El ultimo en ser suscrito se lanza primero
+  btn.removeEventListener('click', alertHandler);// acá lo elimino
+}
+
+// Example #3
+{
+    const container = document.querySelector('.ex3');
+    for(let i=0; i < 9; i++){
+        const brickEl = document.createElement('div');
+        brickEl.classList.add('brick');
+        container.appendChild(brickEl);
+        brickEl.addEventListener('click', function(evt){
+            evt.target.style.backgroundColor = getRandomColor();
+        })
     }
-    
-    btn.onclick = changeBox1Color;
-    btn.onfocus = changeBox1Color;
-    btn.onblur = changeBox1Color;
-    btn.onmouseenter = changeBox1Color;
-    btn.onmouseleave = changeBox1Color;
-
-    //En el window  
-    //window.onkeypress = changeBox1Color;
-    //window.onkeydown = changeBox1Color;
-    //window.onkeyup = changeBox1Color;
-
 }
---
+++ javascript/mdn/objetos.js
@@ -0,0 +1,5 @@
+const c = console.log;
+c('Objetos Basico!!')
+
+const noti = new Notification('Hey you!!');
+c('Notification data',noti.body);
